c49ba26d768d648a274bf5d0c3dc8d4ee08a9671
binukhewage <binukhe@gmail.com>
2024-07-13 20:42:43 +0530

    added git hub to the hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -10,9 +10,9 @@ const Hero = () => {
         <h1><span>I'm Binuk Hewage,</span> Computer Science Undergraduate</h1>
         <p>A 21 years old Computer Science Undergraduate from Informatics Institute of Technology affiliated to University of Westminster London.</p>
         <div class="socialm">
-                <a href="https://instagram.com/binuk.he"><i class='bx bxl-instagram'></i></a>
                 <a href="https://www.linkedin.com/in/binuk-hewage?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"><i class='bx bxl-linkedin-square' ></i></a>
-                <a href="https://x.com/binukhe?s=21&t=ZZSV_r_GMQwmMBG5dY-0xw"><i class='bx bxl-twitter' ></i></a>
+                <a href="https://github.com/binukhewage"><i class='bx bxl-github' ></i></a>
+                <a href="https://instagram.com/binuk.he"><i class='bx bxl-instagram'></i></a>
             </div>
         <div className="hero-action"> 
           <a href={cv} download="Binuk_Hewage_CV.pdf" className="resume">Download CV</a>
